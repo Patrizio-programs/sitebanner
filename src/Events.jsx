339209ba070912ce41dff83f5e459f7978114a38
PatrizioTheDev <73086462+Patrizio-programs@users.noreply.github.com>
2023-05-02 16:51:27 -0400

    Add files via upload

--- src/Events.jsx
+++ src/Events.jsx
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "evo-calendar";
-import $ from "jquery";
+import RevoCalendar from 'revo-calendar'
+import dayjs from "dayjs";
+import { Carousel } from "react-bootstrap";
+import "./Events.css"
 
 function Events() {
   const [events, setEvents] = useState([]);
   const [dataFetched, setDataFetched] = useState(false);
 
+  
   useEffect(() => {
     axios
       .get("https://proxy.cors.sh/https://dyslexiaallianceforblackchildren.org/_functions/calendarEvents", {
@@ -23,46 +26,69 @@ function Events() {
       });
   }, []);
 
-  useEffect(() => {
+  function newEvent(name, date, allDay, extra) {
+    return {
+      name,
+      date,
+      allDay,
+      extra: {
+        text: extra.description,
+        html: extra.courseLinkHtml,
+      },
+    };
+  }
+  
+  const calendarEvents = events.map((event) => {
+    const courseLink = event['link-courses-title'];
+    const courseLinkHtml = courseLink ? `<a href="https://www.dyslexiaallianceforblackchildren.org/${courseLink}" target="_blank">Event Link</a>` : '';
+    const description = event.shortEventDescription;
+    const name = event.title;
+    const dateStr = event.eventDate;
+    const date = dayjs(dateStr).toDate();
+    const allDay = true;
+    const extra = {
+      description,
+      courseLinkHtml,
+    };
+  
+    return newEvent(name, date, allDay, extra);
+  });
+  
+  const revoCalendarProps = {
+    events: calendarEvents,
+    lang: "en",
+    primaryColor: "#4F6995",
+    secondaryColor: "#D7E6EE",
+    todayColor: "#3B3966",
+    textColor: "#333333",
+    indicatorColor: "orange",
+    animationSpeed: 300,
+    sidebarWidth: 180,
+    detailWidth: 280,
+    showDetailToggler: true,
+    showSidebarToggler: true,
+    onePanelAtATime: false,
+    openDetailsOnDateSelection: true,
+    timeFormat24: true,
+    showAllDayLabel: false,
+    detailDateFormat: "YYYY/MM/DD",
+  };
+  
+  return <div>
+  <RevoCalendar {...revoCalendarProps} />
+  <Carousel className="carousel-container" indicators={false}>
+        {calendarEvents.map((event) => (
+          <Carousel.Item key={event.name}>
+            <h4>{event.name}</h4>
+            <p>{event.extra.text}</p>
+            <div dangerouslySetInnerHTML={{ __html: event.extra.html }} />
+            <p>{dayjs(event.date).format("YYYY/MM/DD")}</p>
+          </Carousel.Item>
+        ))}
+      </Carousel>
 
-    const eventLinks = events.map(event => event['link-courses-title']);
 
-    console.log(eventLinks)
-    console.log(events)
-
-    if (dataFetched) {
-      const calendarEvents = events.map((event) => {
-        const courseLink = event['link-courses-title']; // get the course link
-        
-        // create a clickable link with the course link as href
-        const courseLinkHtml = courseLink ? `<a href="https://www.dyslexiaallianceforblackchildren.org/${courseLink}" target="_blank">Course Link</a>` : '';
-        
-        // append the course link to the event description
-        const descriptionWithLink = `${event.shortEventDescription} ${courseLinkHtml}`;
-        
-        return {
-          id: event.id,
-          name: event.title,
-          description: descriptionWithLink,
-          date: event.eventDate,
-          type: "event",
-        };
-      });
-
-     
-      $("#calendar").evoCalendar({
-        format: "yyyy-mm-dd",
-        calendarEvents: calendarEvents,
-      });
-    }
-  }, [dataFetched, events]);
-
-  return (
-    <div>
-      <h1>Events</h1>
-      <div id="calendar"></div>
-    </div>
-  );
-}
+  </div> ;
+      }
 
 export default Events;
